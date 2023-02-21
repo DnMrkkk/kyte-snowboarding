59ec5a786575649e9a2b3ffab3dd1eb5c515e357
Danilo Marcucci <Danimarku00@gmail.com>
2023-02-21 12:24:37 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+/*! 
+    \file main.cpp
+    \author Danilo Marcucci
+    \brief esercizio per calcolare la distanza di un kyte-snowboarder
+    \version 1.0
+    \date 21/02/2023
+
+*/
+
+
+#include <iostream>
+#include <ctime>
+#include <fstream>
+
+/*!
+    \struct s_info
+    \brief per l'implementazine software delle caratteristiche del kyte-snowboarder
+*/
+struct s_info{
+    int matricola;
+    string cognome;
+    int xy[30][30];
+}partecipanti[20];
+
+using namespace std;
+
+
+/*!
+    \fn menu
+    \brief menu'
+*/
+short menu(){
+    int scelta;
+}
+
+/*!
+    \fn main
+    \brief dove tutto ebbe inizio
+*/
+int main(){
+    menu();
+    return 0;
+}
