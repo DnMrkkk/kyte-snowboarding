432d0407daa757889745ea09228c31cfbd3e5515
Danilo Marcucci <Danimarku00@gmail.com>
2023-02-25 17:54:37 +0100

    continuo esercizio

--- main.cpp
+++ main.cpp
@@ -11,18 +11,61 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
+#include <math.h>
+#define FILE_NAME "dati.txt"
+
+using namespace std;
+
 
 /*!
     \struct s_info
     \brief per l'implementazine software delle caratteristiche del kyte-snowboarder
 */
 struct s_info{
-    int matricola;
-    string cognome;
-    int xy[30][30];
+    public:
+        int matricola;
+        string cognome;
+        int x[30];
+        int y[30];
+        int tot_dist = 0;
 }partecipanti[20];
+int n_part = 0;
 
-using namespace std;
+
+/*!
+    \fn inserimento
+    \brief funzione per l'inserimento dei partecipanti
+*/
+void inserimento(){
+
+    cout << "Quanti partecipanti vuoi inserire?(max 20): ";
+    cin >> n_part;
+    for(int i = 1; i <= n_part; i++){
+        cout << "Inserisci cognome del " << i << " partecipante: ";
+        string c;
+        cin >> c;
+        partecipanti[i-1].cognome = c;
+        partecipanti[i-1].matricola = i;
+    }
+}
+
+/*!
+    \fn avvia_gara
+    \brief funzione per la rilevazione del percorso dei partecipanti
+*/
+void avvia_gara(){
+    for(int i = 0; i < n_part; i++ ){
+        for(int c = 0; c < rand()%16+15; c++){
+            partecipanti[i].x[c] = rand()%101;
+            partecipanti[i].y[c] = rand()%101;
+            partecipanti[i].tot_dist += sqrt(pow(partecipanti[i].x[c], 2) + pow(partecipanti[i].y[c] = rand()%101, 2));
+        }
+    }
+
+
+    
+}
+
 
 
 /*!
@@ -31,6 +74,23 @@ using namespace std;
 */
 short menu(){
     int scelta;
+    while(true){
+        cout << "1-Inserisci cognome partecipante(max 20)\n2-Avvia gara\n3-Visualizza classifica\n4-Esci\n>> ";
+        cin >> scelta;
+        switch(scelta){
+            case 1:
+                inserimento();
+                break;
+            case 2:
+                avvia_gara();
+                break;
+            case 3:
+
+                break;
+            case 4:
+                return 0;
+        }
+    }
 }
 
 /*!
